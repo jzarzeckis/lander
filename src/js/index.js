5fe32ba870d58d4d35d9d5b0a83ab542b1cc61d2
Janis Zarzeckis <jzarzeckis@evolutiongaming.com>
2019-07-10 21:08:18 +0300

    Add thrusters

--- src/js/index.js
+++ src/js/index.js
@@ -25,12 +25,50 @@ loader.add('rocket', img.rocket).load((loader, resources) => {
     rocket.anchor.x = 0.5;
     rocket.anchor.y = 0.65;
  
-    // Add the bunny to the scene we are building.
+    // Add the rocket to the scene we are building.
     app.stage.addChild(rocket);
+    let vy = 0;
+    let vx = 0;
+    let isEngineRunning = false;
+    let isRightThrusterRunning = false;
+    let isLeftThrusterRunning = false;
+    document.addEventListener("keydown", (e) => {
+        e.preventDefault();
+        console.log(e);
+        if (e.code === "ArrowUp") {
+            isEngineRunning = true;
+        } else if (e.code === "ArrowRight") {
+            isRightThrusterRunning = true
+        } else if (e.code === "ArrowLeft") {
+            isLeftThrusterRunning = true
+        }
+    });
+    document.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        if (e.code === "ArrowUp") {
+            isEngineRunning = false;
+        } else if (e.code === "ArrowRight") {
+            isRightThrusterRunning = false;
+        } else if (e.code === "ArrowLeft") {
+            isLeftThrusterRunning = false;
+        }
+    })
  
     // Listen for frame updates
     app.ticker.add(() => {
          // each frame we spin the rocket around a bit
-        rocket.rotation += 0.01;
+        // rocket.rotation += 0.01;
+        rocket.y = rocket.y + vy;
+        rocket.x -= vx;
+        vy = vy + 0.01; // Gravity is pulling us down
+        if (isEngineRunning) {
+            vy -= 0.03;
+        }
+        if (isLeftThrusterRunning) {
+            vx += 0.04;
+        }
+        if (isRightThrusterRunning) {
+            vx -= 0.04;
+        }
     });
 });
